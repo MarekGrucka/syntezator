91a14756fc038e6da1ee879f6a3371c53e4dfcab
Eyescream <marek.grucka@gmail.com>
2019-07-22 20:40:31 +0200

    Git init

---
+++ src/main/java/com/syntezator/syntezator/SyntezatorApplication.java
@@ -0,0 +1,13 @@
+package com.syntezator.syntezator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntezatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntezatorApplication.class, args);
+	}
+
+}
